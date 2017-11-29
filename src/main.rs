38bd8cad034cd6737f5fa7e7202d48c6838f1edc
Pedro Scaff <pedro@scaff.me>
2017-11-29 20:12:59 +0100

    implement basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("geoshaper")
+    					.version("0.1.0")
+    					.author("Pedro Scaff <email>")
+    					.arg(Arg::with_name("image")
+    						.short("i")
+    						.long("image")
+    						.value_name("FILE")
+    						.help("target image")
+    						.takes_value(true)
+    						.required(true))
+    					.get_matches();
+    let img = matches.value_of("image").unwrap();
+    println!("Target image -> {}", img);
 }
