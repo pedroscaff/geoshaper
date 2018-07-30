380236e82d668228b88335ed0b7c30e7667c6fa5
Pedro Scaff <pedro@scaff.me>
2018-07-30 13:15:15 +0200

    Refactor scale

--- src/rectangle.rs
+++ src/rectangle.rs
@@ -49,6 +49,17 @@ impl Shape for Rectangle {
             y: (points[3].y + points[0].y) / 2.0
         }
     }
+
+    fn scale(points: &mut Vec<Point>, scale_x: &f32, scale_y: &f32) {
+        let current_width = points[1].x - points[0].x;
+        let current_height = points[3].y - points[0].y;
+        let scaled_width = points[0].x + current_width * scale_x;
+        let scaled_height = points[0].y + current_height * scale_y;
+        points[1].x = scaled_width;
+        points[2].x = scaled_width;
+        points[2].y = scaled_height;
+        points[3].y = scaled_height;
+    }
 }
 
 #[cfg(test)]
@@ -62,5 +73,39 @@ mod tests {
             assert!(point.x < 512.0);
             assert!(point.y < 512.0);
         }
+    }
+
+    #[test]
+    fn should_scale() {
+        let p0 = Point {
+            x: 0.0,
+            y: 0.0,
+        };
+        let p1 = Point {
+            x: 5.0,
+            y: 0.0,
+        };
+        let p2 = Point {
+            x: 5.0,
+            y: 5.0,
+        };
+        let p3 = Point {
+            x: 0.0,
+            y: 5.0,
+        };
+        let mut points = vec![p0, p1, p2, p3];
+        Rectangle::scale(&mut points, &2.0, &2.0);
+        // should remain the same
+        assert!(points[0].x == 0.0);
+        assert!(points[0].y == 0.0);
+        // x should change
+        assert!(points[1].x == 10.0);
+        assert!(points[1].y == 0.0);
+        // both should change
+        assert!(points[2].x == 10.0);
+        assert!(points[2].y == 10.0);
+        // y should change
+        assert!(points[3].x == 0.0);
+        assert!(points[3].y == 10.0);
     }
 }
